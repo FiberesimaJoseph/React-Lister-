50e35050ffd5a901c0b094f3604dc2fadeba089e
FiberesimaJoseph <sctbilly@gmail.com>
2020-08-04 07:47:59 -0600

    new update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputTextArea from "./component/InputTextArea";
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To Do List{item}</h1>
+        <button></button>
+      </div>
+      <InputTextArea />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/InputTextArea.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const InputTextArea = () => {
+  return (
+    <div className="form">
+      <input type="text"></input>
+      <button>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+};
+
+export default InputTextArea;
